73ea0b03b84d6ebb108290979026ee26d592fd4d
Raian Barbosa <raianpbstudio@gmail.com>
2025-05-26 20:58:14 -0300

    atualizando swagger

--- README.md
+++ README.md
@@ -2,6 +2,61 @@
 
 ## Em construção
 
+## Como iniciar o projeto
+
+### Pré-requisitos
+
+- Python 3.8+
+- Docker e Docker Compose (opcional)
+- pip
+
+### Instalação local
+
+1. Clone o repositório:
+   ```bash
+   git clone https://github.com/seu-usuario/base-system-flask.git
+   cd base-system-flask
+   ```
+
+2. Crie um ambiente virtual:
+   ```bash
+   python -m venv venv
+   source venv/bin/activate  # Linux/Mac
+   venv\Scripts\activate     # Windows
+   ```
+
+3. Instale as dependências:
+   ```bash
+   pip install -r requirements.txt
+   ```
+
+4. Inicie o servidor Flask:
+   ```bash
+   python routes.py
+   ```
+
+5. Acesse a documentação Swagger:
+   ```
+   http://localhost:8000/apidocs/
+   ```
+
+### Usando Docker
+
+1. Suba os containers:
+   ```bash
+   docker-compose up
+   ```
+
+2. Acesse a API e a documentação Swagger normalmente:
+   ```
+   http://localhost:8000/apidocs/
+   ```
+
+## Observações
+
+- Para autenticação JWT, faça login na rota `/login` e use o token retornado no botão "Authorize" do Swagger, sempre com o prefixo `Bearer `.
+- As variáveis de ambiente podem ser configuradas em um arquivo `.env` se necessário.
+
 ### Comandos
 
 `docker-compose up`
--- routes.py
+++ routes.py
@@ -1,21 +1,74 @@
-
 import uuid
 from serializer.to_json import serial
 from models.userModel import Account , db, api
 from flask_jwt_extended import create_access_token, jwt_required
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask import request, jsonify
+from flask import Flask
+from flasgger import Swagger
 
-
+# Configuração do Swagger para JWT Bearer Auth
+swagger_template = {
+    "swagger": "2.0",
+    "info": {
+        "title": "Base System API",
+        "description": "Base System Flask",
+        "version": "1.0"
+    },
+    "securityDefinitions": {
+        "Bearer": {
+            "type": "apiKey",
+            "name": "Authorization",
+            "in": "header",
+            "description": "Insira o token JWT com o prefixo 'Bearer '. Exemplo: Bearer seu_token_aqui"
+        }
+    }
+}
+swagger = Swagger(api, template=swagger_template)
 
 @api.get('/')
 @jwt_required()
 def init():
+    """
+    Teste de conexão com a API.
+    ---
+    tags:
+      - Ordens de Serviço
+    security:
+      - Bearer: []
+    responses:
+      200:
+        description: Conexão bem-sucedida
+    """
     msg = "Api v1 Connection Success"
     return serial(200, msg)
 
 @api.route("/login", methods=["POST"])
 def login():
+    """
+    Login do usuário.
+    ---
+    tags:
+      - Acesso
+    parameters:
+      - in: body
+        name: body
+        schema:
+          type: object
+          required:
+            - username
+            - password
+          properties:
+            username:
+              type: string
+            password:
+              type: string
+    responses:
+      200:
+        description: Login realizado com sucesso
+      401:
+        description: Usuário ou senha incorretos
+    """
     
     data = request.get_json()
     query = Account.query.filter_by(username=data["username"]).first()
@@ -35,7 +88,8 @@ def login():
             user.online = True
             db.session.merge(user)
             db.session.commit()
-            access_token = create_access_token(identity=pwd)
+            access_token = "Bearer"+" "+create_access_token(identity=pwd)
+            
             msg ='ok'
         return serial(200, msg, access_token)
     
@@ -47,6 +101,37 @@ def login():
 
 @api.route('/v1/account/users', methods=['POST'])
 def account():
+    """
+    Criação de novo usuário.
+    ---
+    tags:
+      - Acesso
+    parameters:
+      - in: body
+        name: body
+        schema:
+          type: object
+          required:
+            - fullname
+            - username
+            - password
+            - age
+            - uf
+          properties:
+            fullname:
+              type: string
+            username:
+              type: string
+            password:
+              type: string
+            age:
+              type: integer
+            uf:
+              type: string
+    responses:
+      200:
+        description: Usuário criado com sucesso
+    """
     data = request.get_json()
     gen_pass = str(generate_password_hash(data["password"]))
     uuidone = str(uuid.uuid4())
@@ -66,6 +151,17 @@ def account():
 @api.route('/v1/account/users', methods=['GET'])
 @jwt_required()
 def selectAccount():
+    """
+    Lista todos os usuários.
+    ---
+    tags:
+      - Acesso
+    security:
+      - Bearer: []
+    responses:
+      200:
+        description: Lista de usuários
+    """
     cursor = Account.query.all()
     user_json = [Account.selectUser() for Account in cursor]
     return serial(200, user_json)
@@ -73,6 +169,22 @@ def selectAccount():
 @api.route('/v1/account/user/<id>', methods=['GET'])
 @jwt_required()
 def getByIdAccount(id):
+    """
+    Busca usuário por ID.
+    ---
+    tags:
+      - Acesso
+    security:
+      - Bearer: []
+    parameters:
+      - in: path
+        name: id
+        type: string
+        required: true
+    responses:
+      200:
+        description: Usuário encontrado
+    """
     query = Account.query.filter_by(id=id).first()
     query = query.userGetby()
     return serial(200, query)
@@ -80,6 +192,34 @@ def getByIdAccount(id):
 @api.route('/v1/account/user/<id>', methods=['PUT'])
 @jwt_required()
 def putByAccount(id):
+    """
+    Atualiza dados do usuário.
+    ---
+    tags:
+      - Acesso
+    security:
+      - Bearer: []
+    parameters:
+      - in: path
+        name: id
+        type: string
+        required: true
+      - in: body
+        name: body
+        schema:
+          type: object
+          required:
+            - fullname
+            - age
+          properties:
+            fullname:
+              type: string
+            age:
+              type: integer
+    responses:
+      200:
+        description: Atualização realizada com sucesso
+    """
     data = request.get_json()
     user = Account.query.filter_by(id=id).first()
     
@@ -95,7 +235,22 @@ def putByAccount(id):
 @api.route('/v1/account/user/<id>', methods=['DELETE'])
 @jwt_required()
 def deleteAccount(id):
-
+    """
+    Remove usuário por ID.
+    ---
+    tags:
+      - Ordens de Serviço
+    security:
+      - Bearer: []
+    parameters:
+      - in: path
+        name: id
+        type: string
+        required: true
+    responses:
+      200:
+        description: Usuário removido com sucesso
+    """
     user = Account.query.filter_by(id=id).first()
 
     db.session.delete(user)
@@ -105,6 +260,20 @@ def deleteAccount(id):
 
 @api.route('/logout/<id>', methods=["GET"])
 def logout(id):
+    """
+    Logout do usuário.
+    ---
+    tags:
+      - Ordens de Serviço
+    parameters:
+      - in: path
+        name: id
+        type: string
+        required: true
+    responses:
+      200:
+        description: Logout realizado com sucesso
+    """
     
     user = Account.query.filter_by(id=id).first()
     user.online = False
